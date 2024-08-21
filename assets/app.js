e3ac0cb7931d8bed710e11d993afb2a50347d758
Jean Coppieters <jean.coppieters@kiwim.app>
2024-08-21 18:01:17 +0100

    Adicionando react

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@pages/App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
